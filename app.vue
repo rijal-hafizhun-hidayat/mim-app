c83d4711ca7dfb16760a21ccc08bb945810552b5
unknown <rijal.1344@gmail.com>
2024-11-17 05:48:00 +0700

    dependencies: add tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+useHead({
+  title: "mim-app",
+});
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
