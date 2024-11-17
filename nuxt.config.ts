a9aa0353d9f5a7b6cd67b7958d45022550bf9d4d
unknown <rijal.1344@gmail.com>
2024-11-17 20:55:07 +0700

    page: add meme page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  css: ["vue-multiselect/dist/vue-multiselect.min.css"],
   modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts", "@formkit/auto-animate/nuxt"],
 });
