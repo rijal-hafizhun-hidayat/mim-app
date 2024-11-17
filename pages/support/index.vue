df80607b90818f9ac83837548514ef261b2c8e00
unknown <rijal.1344@gmail.com>
2024-11-17 14:08:14 +0700

    page: adjust font size in mobile version

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -5,8 +5,11 @@ console.log(true);
   <NuxtLayout name="landing-layout">
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="mt-12 px-4 py-6 space-y-8">
-        <div class="text-center">
-          <p class="text-slate-900 text-2xl">
+        <div class="text-center space-y-6">
+          <h1 class="text-4xl">
+            Support <span class="uppercase font-bold">mim</span>
+          </h1>
+          <p class="text-slate-900 text-xs sm:text-sm">
             Mim will always be free for everyone. However, if you would like to
             support the platform, your support would be greatly appreciated as
             it helps cover development and server costs. Thank you so much!
