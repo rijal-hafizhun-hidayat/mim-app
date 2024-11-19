a89ced8a8ae7db4da74e6d651f0e55ca4097e642
unknown <rijal.1344@gmail.com>
2024-11-19 12:41:17 +0700

    page: add meme type section in meme page

--- components/meme/More.vue
+++ components/meme/More.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="mt-10 px-4 py-4 overflow-x-auto border hover:bg-slate-100 cursor-pointer active:bg-slate-200 transition ease-in-out duration-150"
+    class="mt-10 px-4 py-4 overflow-x-auto border hover:bg-slate-100 cursor-pointer active:bg-slate-200 active:outline-none active:ring-2 active:ring-black active:ring-offset-2 transition ease-in-out duration-150"
   >
     <p class="text-center font-semibold capitalize">tampil lebih banyak</p>
   </div>
