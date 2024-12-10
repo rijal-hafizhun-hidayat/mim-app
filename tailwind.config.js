318b418e427d413fa5aba9973adf91713ede47d6
unknown <rijal.1344@gmail.com>
2024-12-10 15:29:04 +0700

    refactor: import module using es6 in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+import aspectRatio from '@tailwindcss/aspect-ratio';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
@@ -5,9 +9,9 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    typography,
+    forms,
+    aspectRatio,
   ],
 }
 
